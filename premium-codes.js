790daca7b3d28b25fb7d545f15cf0842c89e5f3e
abdellah <abdollahhammou66@gmail.com>
2026-08-07 22:48:29 +0100

    code

---
+++ premium-codes.js
@@ -0,0 +1,15 @@
+/**
+ * PREMIUM ACTIVATION CODES
+ * 
+ * You can add, remove, or modify valid codes in this list.
+ * Any code added here will work in the app to remove ads and grant full premium access.
+ */
+window.VALID_PREMIUM_CODES = [
+  "PREMIUM2026",
+  "NOADS100",
+  "VIPACCESS",
+  "LEXICONPRO",
+  "LEX2026",
+  "FREEPASS",
+  "INSTAGRAM2026"
+];
